bcc6eb5716fda8599ae1dbc5403a07335a4b0de2
Anoop B <16367697+anoop-b@users.noreply.github.com>
2021-06-02 19:41:31 +0530

    Refactor: change function signature

--- middlewares/cacheHandler.go
+++ middlewares/cacheHandler.go
@@ -9,18 +9,21 @@ import (
 	"net/http"
 )
 
-func CacheCheck(c *gin.Context) {
-	ifsc, err := helpers.ValidIfsc(c.Param("ifsc"))
-	c.Set("sanitisedIFSC", ifsc)
-	if !err {
-		c.AbortWithStatusJSON(http.StatusNotFound, "Not a Valid IFSC")
-	}
-	response, exists := helpers.GetCache(ifsc)
-	if exists {
-		log.Println("cache hit")
-		c.SecureJSON(http.StatusOK, response)
-		c.Abort()
-	} else {
-		c.Next()
+func CacheCheck() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		ifsc, err := helpers.ValidIfsc(c.Param("ifsc"))
+		c.Set("sanitisedIFSC", ifsc)
+		if !err {
+			c.AbortWithStatusJSON(http.StatusNotFound, "Not a Valid IFSC")
+		}
+		response, exists := helpers.GetCache(ifsc)
+		if exists {
+			log.Println("cache hit")
+			c.SecureJSON(http.StatusOK, response)
+			c.Abort()
+		} else {
+			c.Next()
+		}
 	}
 }
