95fca87ca70a5c7b6e166bfd001b4ef2bde74bfd
Anoop B <16367697+anoop-b@users.noreply.github.com>
2021-06-14 20:37:04 +0530

    Refactor: Define caching interface

--- helpers/cache.go
+++ helpers/cache.go
@@ -1,20 +1,34 @@
 package helpers
 
 import (
+	"github.com/pmylund/go-cache"
 	"log"
 	"time"
-
-	"github.com/pmylund/go-cache"
 )
+
+type CacheService interface {
+	GetCache(key string) (interface{}, bool)
+	SetCache(key string, response interface{})
+}
+
+type cacheServer struct {
+	cacheServer *cache.Cache
+}
+
+func NewCacheServer() *cacheServer {
+	return &cacheServer{
+		ck,
+	}
+}
 
 //using go-cache for caching, but can be easily swapped with redis/memcached etc
 var ck = cache.New(24*time.Hour, 12*time.Hour)
 
-func GetCache(ifsc string) (interface{}, bool) {
-	return ck.Get(ifsc)
+func (c *cacheServer) GetCache(ifsc string) (interface{}, bool) {
+	return c.cacheServer.Get(ifsc)
 }
 
-func SetCache(ifsc string, response interface{}) {
+func (c *cacheServer) SetCache(ifsc string, response interface{}) {
 	log.Println("cache set")
-	ck.Set(ifsc, response, 0)
+	c.cacheServer.Set(ifsc, response, 0)
 }
