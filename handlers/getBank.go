0aac13836fa315c58307fa49060c6389f6217b02
Anoop B <16367697+anoop-b@users.noreply.github.com>
2021-06-16 13:00:26 +0530

    feat: swap localstorage with embedded Data folder

--- handlers/getBank.go
+++ handlers/getBank.go
@@ -1,51 +1,42 @@
 package handlers
 
 import (
+	"embed"
 	"encoding/json"
+	"github.com/gin-gonic/gin"
 	"go-ifsc/helpers"
 	"go-ifsc/models"
 
-	"github.com/gin-gonic/gin"
-
-	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
-	"path/filepath"
 )
 
 //GetBank is the default handler func for fetching IFSC
-func GetBank(c *gin.Context) {
-	var banks map[string]models.Bank
-	ifsc := c.MustGet("sanitisedIFSC").(string)
-	bankCode := ifsc[0:4]
-
-	jsonFilePath := filepath.Join("Data", bankCode+".json")
-
-	if _, err := os.Stat(jsonFilePath); os.IsNotExist(err) {
-		c.AbortWithStatusJSON(http.StatusNotFound, "Not Found")
-		return
-	}
-	jsonFile, err := os.Open(jsonFilePath)
-	if err != nil {
-		log.Fatalln("error opening json file", err)
-	}
-	defer jsonFile.Close()
-
-	byteValue, _ := ioutil.ReadAll(jsonFile)
-	err = json.Unmarshal(byteValue, &banks)
-	if err != nil {
-		log.Fatalln("Failed to unmarshal", err)
-	}
-
-	if ifsc != "" {
-		payload, exists := banks[ifsc]
-		if exists {
-			c.SecureJSON(http.StatusOK, payload)
-			ck := helpers.NewCacheServer()
-			ck.SetCache(ifsc, payload)
+func GetBank(directory *embed.FS) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var banks map[string]models.Bank
+		ifsc := c.MustGet("sanitisedIFSC").(string)
+		bankCode := ifsc[0:4]
+		jsonObject, err := directory.ReadFile("Data/" + bankCode + ".json")
+		if os.IsNotExist(err) {
+			c.AbortWithStatusJSON(http.StatusNotFound, "Not Found")
 			return
 		}
-		c.AbortWithStatusJSON(http.StatusNotFound, "Not Found")
+		err = json.Unmarshal(jsonObject, &banks)
+		if err != nil {
+			log.Fatalln("Failed to unmarshal", err)
+		}
+
+		if ifsc != "" {
+			payload, exists := banks[ifsc]
+			if exists {
+				c.SecureJSON(http.StatusOK, payload)
+				ck := helpers.NewCacheServer()
+				ck.SetCache(ifsc, payload)
+				return
+			}
+			c.AbortWithStatusJSON(http.StatusNotFound, "Not Found")
+		}
 	}
 }
